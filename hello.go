7f23483e1075462b6a4de9608a48ee3c1b8e4762
cyf <cyf@MateBook-X-Pro.local>
2023-10-28 15:12:03 +0800

    x

--- hello.go
+++ hello.go
@@ -4,54 +4,27 @@ import (
 	"fmt"
 )
 
-type MyStack struct {
-	queue []int
+type ListNode struct {
+	Val  int
+	Next *ListNode
 }
 
-
-func Constructor() MyStack {
-	return MyStack{
-		queue: make([]int, 0),
+func detectCycle(head *ListNode) *ListNode {
+	slow, fast := head, head
+	for fast != nil && fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+		if slow == fast {
+			slow = head
+			for slow != fast {
+				slow = slow.Next
+				fast = fast.Next
+			}
+			return slow
+		}
 	}
+	return nil
 }
-
-
-func (this *MyStack) Push(x int)  {
-	this.queue = append(this.queue, x)
-}
-
-
-func (this *MyStack) Pop() int {
-	length := len(this.queue)
-	for length > 1 {
-		front := this.queue[0]
-		this.queue = this.queue[1:]
-		this.Push(front)
-	}
-	res := this.queue[0]
-	this.queue = this.queue[1:]
-	return res
-}
-
-
-func (this *MyStack) Top() int {
-	return this.queue[0]
-}
-
-
-func (this *MyStack) Empty() bool {
-	return len(this.queue) == 0
-}
-
-
-/**
- * Your MyStack object will be instantiated and called as such:
- * obj := Constructor();
- * obj.Push(x);
- * param_2 := obj.Pop();
- * param_3 := obj.Top();
- * param_4 := obj.Empty();
- */
 
 func main() {
 	fmt.Print("Hi")
