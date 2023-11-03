05ea23b2010bfbac666e8b09da87c04b94bfc686
cyf <cyf@MateBook-X-Pro.local>
2023-11-03 16:04:58 +0800

    d

--- hello.go
+++ hello.go
@@ -4,38 +4,26 @@ import (
 	"fmt"
 )
 
-func getNext(next []int, s string) {
-	j := 0
-	next[0] = j
-	for i := 1; i < len(s); i++ {
-		//随便举个例子'acaacac'画一画清楚一点
-		for j > 0 && s[i] != s[j] {
-			j = next[j-1]
-		}
-		if s[i] == s[j] {
-			j++
-		}
-		next[i] = j
-	}
-}
-
-func strStr(haystack string, needle string) int {
-	l := len(needle)
-	next := make([]int, l)
-	getNext(next, needle)
-	j := 0
-	for i := 0; i < len(haystack); i++ {
-		for j > 0 && haystack[i] != needle[j] {
-			j = next[j-1]
-		}
-		if haystack[i] == needle[j] {
-			j++
-		}
-		if j == l {
-			return i - l + 1
+func isValid(s string) bool {
+	set := map[byte]byte{')': '(', '}': '{', ']': '['}
+	stack := make([]byte, 0)
+	for i := 0; i < len(s); i++ {
+		ch := s[i]
+		if ch == '(' || ch == '{' || ch == '[' {
+			stack = append(stack, ch)
+		} else {
+			l := len(stack) - 1
+			if l < 0 {
+				return false
+			}
+			if stack[l] == set[ch] {
+				stack = stack[:l]
+			} else {
+				return false
+			}
 		}
 	}
-	return -1
+	return len(stack) == 0
 }
 
 func main() {
