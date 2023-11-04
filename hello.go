beee18330649495b9eecf338d2ea3b528633c3b0
cyf <cyf@MateBook-X-Pro.local>
2023-11-04 12:08:10 +0800

    x

--- hello.go
+++ hello.go
@@ -4,26 +4,8 @@ import (
 	"fmt"
 )
 
-func isValid(s string) bool {
-	set := map[byte]byte{')': '(', '}': '{', ']': '['}
-	stack := make([]byte, 0)
-	for i := 0; i < len(s); i++ {
-		ch := s[i]
-		if ch == '(' || ch == '{' || ch == '[' {
-			stack = append(stack, ch)
-		} else {
-			l := len(stack) - 1
-			if l < 0 {
-				return false
-			}
-			if stack[l] == set[ch] {
-				stack = stack[:l]
-			} else {
-				return false
-			}
-		}
-	}
-	return len(stack) == 0
+func topKFrequent(nums []int, k int) []int {
+	
 }
 
 func main() {
