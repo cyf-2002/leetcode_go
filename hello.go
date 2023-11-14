52e23cbf021e2d3fbc4a60c9c7554b6c2233038e
yifan chen <865636335@qq.com>
2023-11-14 10:21:46 +0800

    s

--- hello.go
+++ hello.go
@@ -2,38 +2,38 @@ package main
 
 import (
 	"fmt"
-	"math"
 )
 
-// Definition for a binary tree node.
-type TreeNode struct {
-	Val   int
-	Left  *TreeNode
-	Right *TreeNode
-}
+func combinationSum3(n int, k int) [][]int {
+	path := make([]int, 0, k)
+	res := make([][]int, 0)
 
-func getMinimumDifference(root *TreeNode) int {
-	var nums []int
-
-	var traversal func(root *TreeNode)
-	traversal = func(root *TreeNode) {
-		if root == nil {
+	var backtracking func(n, k, start int)
+	backtracking = func(n, k, start int) {
+		if n == 0 && k == 0 {
+			tmp := append([]int{}, path...)		//拷贝
+			res = append(res, tmp)
 			return
 		}
-		traversal(root.Left)
-		nums = append(nums, root.Val)
-		traversal(root.Right)
-	}
-	traversal(root)
-	res := math.MaxInt64
-	for i := 1; i < len(nums); i++ {
-		if nums[i]-nums[i-1] < res {
-			res = nums[i]-nums[i-1]
+
+		for i := start; i <= 9; i++ {
+			if i > n {
+				break
+			}
+			if 9-i+1 < k-len(path) {	//剪枝
+				break
+			}
+			path = append(path, i)
+			backtracking(n-i, k-1, i+1)
+			path = path[:len(path)-1]	//回溯
 		}
 	}
+
+	backtracking(n, k, 1)
 	return res
 }
 
 func main() {
-	fmt.Print("hello")
+	res := combinationSum3(1, 4)
+	fmt.Print(res)
 }
