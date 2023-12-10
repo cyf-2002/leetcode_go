b6977f44137ff2bf221fca97075ce31ca6197fca
yifan chen <865636335@qq.com>
2023-12-10 19:34:02 +0800

    dp

--- hello.go
+++ hello.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"fmt"
-
+	"github.com/emirpasic/gods/sets/treeset"
 )
 
-
-
 func main() {
+	set := treeset.NewWithIntComparator()
+	set.Add(1)
+	set.Add(3, 4, 5, 2)
+	fmt.Print(set)
 	fmt.Print("hi")
 }
