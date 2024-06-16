e0837e5150de48f5b948110cb1b0a00d3cf5f3c0
cyf-2002 <865636335@qq.com>
2024-06-17 02:55:20 +0800

    update

--- hello.go
+++ hello.go
@@ -3,5 +3,45 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, World!")
+	c := search([]int{1, 3}, 3)
+	fmt.Println(c)
+}
+
+func search(nums []int, target int) int {
+	n := len(nums)
+	if n == 1 {
+		if target == nums[0] {
+			return 0
+		}
+		return -1
+	}
+	// 找到第一段和第二段中间点的下标l
+	l, r := 0, n-1
+	for l < r {
+		mid := (l + r) / 2
+		if nums[mid] > nums[r] {
+			l = mid + 1
+		} else {
+			r = mid
+		}
+	}
+	// 判断target是在第一段还是第二段
+	if target >= nums[0] {
+		l = 0
+	} else {
+		r = n - 1
+	}
+	// 二分查找
+	for l <= r {
+		mid := (l + r) / 2
+		if nums[mid] == target {
+			return mid
+		}
+		if nums[mid] > target {
+			r = mid - 1
+		} else {
+			l = mid + 1
+		}
+	}
+	return -1
 }
