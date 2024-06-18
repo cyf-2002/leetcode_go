c6da1cf88fa9f0d2e96d15958dd57f9981ee604a
cyf-2002 <865636335@qq.com>
2024-06-18 12:02:21 +0800

    update

--- hello.go
+++ hello.go
@@ -1,47 +1,49 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
 
 func main() {
-	c := search([]int{1, 3}, 3)
-	fmt.Println(c)
+
+	fmt.Println(rand.Intn(10))
+	fmt.Println(rand.Intn(10))
+	fmt.Println(rand.Intn(10))
+	fmt.Println(rand.Intn(10))
+
 }
 
-func search(nums []int, target int) int {
-	n := len(nums)
-	if n == 1 {
-		if target == nums[0] {
-			return 0
-		}
-		return -1
+func sortArray(nums []int) []int {
+	quickSort(nums, 0, len(nums)-1)
+	return nums
+}
+
+func quickSort(nums []int, l, r int) {
+	if l < r {
+		pivot := partition(nums, l, r)
+		quickSort(nums, l, pivot-1)
+		quickSort(nums, pivot+1, r)
 	}
-	// 找到第一段和第二段中间点的下标l
-	l, r := 0, n-1
-	for l < r {
-		mid := (l + r) / 2
-		if nums[mid] > nums[r] {
-			l = mid + 1
-		} else {
-			r = mid
+}
+
+func partition(nums []int, l, r int) int {
+	newRand := rand.New(rand.NewSource(time.Now().UnixNano()))
+	// 将pivot移到最右
+	keyIndex := l + newRand.Intn(r-l+1)
+	key := nums[keyIndex]
+	nums[keyIndex], nums[r] = nums[r], nums[keyIndex]
+	// [l, i) < key [i, r] >= key
+	i, j := l, l
+	for j < r {
+		if nums[j] < key {
+			nums[i], nums[j] = nums[j], nums[i]
+			i++
 		}
+		j++
 	}
-	// 判断target是在第一段还是第二段
-	if target >= nums[0] {
-		l = 0
-	} else {
-		r = n - 1
-	}
-	// 二分查找
-	for l <= r {
-		mid := (l + r) / 2
-		if nums[mid] == target {
-			return mid
-		}
-		if nums[mid] > target {
-			r = mid - 1
-		} else {
-			l = mid + 1
-		}
-	}
-	return -1
+	// 将pivot放到i处
+	nums[i], nums[r] = nums[r], nums[i]
+	return i
 }
