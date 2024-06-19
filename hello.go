d75f56e3bde55a039f44b53a340e15a5461db3c5
cyf-2002 <865636335@qq.com>
2024-06-19 12:17:25 +0800

    update

--- hello.go
+++ hello.go
@@ -5,38 +5,11 @@ import (
 )
 
 func main() {
-	c := findKthLargest([]int{1, 3, 4, 2, 6, 5}, 2)
-	fmt.Println(c)
+	var a, b int
+	_, err := fmt.Scan(&a, &b)
+	if err != nil {
+		return
+	}
+	fmt.Println(a, b)
 
-}
-
-func findKthLargest(nums []int, k int) int {
-	heapSize := len(nums)
-	buildMaxHeap(nums, heapSize)
-	for i := len(nums) - 1; i >= len(nums)-k+1; i-- {
-		nums[0], nums[i] = nums[i], nums[0]
-		heapSize--
-		maxHeapify(nums, 0, heapSize)
-	}
-	return nums[0]
-}
-
-func buildMaxHeap(a []int, heapSize int) {
-	for i := heapSize / 2; i >= 0; i-- {
-		maxHeapify(a, i, heapSize)
-	}
-}
-
-func maxHeapify(a []int, i, heapSize int) {
-	l, r, largest := i*2+1, i*2+2, i
-	if l < heapSize && a[l] > a[largest] {
-		largest = l
-	}
-	if r < heapSize && a[r] > a[largest] {
-		largest = r
-	}
-	if largest != i {
-		a[i], a[largest] = a[largest], a[i]
-		maxHeapify(a, largest, heapSize)
-	}
 }
